24d93755a88103a7a073fd0d87f38ea062f1d4db
TheNerd123 <richardyin000@gmail.com>
2023-11-19 15:22:34 -0800

    completed the skeleton of the project

--- code/ComplexPlane.h
+++ code/ComplexPlane.h
@@ -1,6 +1,7 @@
 #ifndef COMPLEXPLANE_H
 #define COMPLEXPLANE_H
 
+// Include important C++ libraries here
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
@@ -8,11 +9,43 @@
 #include <vector>
 #include <complex>
 
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+enum State {CALCULATING, DISPLAYING}; 
+
 class ComplexPlane {
 public: 
-    char test();
+    //public functions:
+    ComplexPlane(int pixelWidth, int pixelHeight);
+    void draw(RenderTarget& target, RenderStates states) const;
+    void zoomIn();
+    void zoomOut();
+    void setCenter(Vector2i mousePixel);
+    void setMouseLocation(Vector2i mousePixel);
+    void loadText(Text& text);
+    void updateRender();
+
 private:
-    int x = 0;
+    //private members:
+    State m_state;
+    VertexArray m_vArray;
+    Vector2f m_mouseLocation;
+    Vector2f m_plane_center;
+    Vector2f m_place_size;
+    Vector2i m_pixel_size;
+    int m_zoomCount;
+    float m_aspectRatio;
+
+    //private functions:
+    int countIterations(Vector2f coord);
+    void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+    Vector2f mapPixelToCoords(Vector2i mousePixel);
 };
 
 
--- code/main.cpp
+++ code/main.cpp
@@ -1,22 +1,12 @@
 //Created by Richard Chao and Dominic Agoncillo
 //Created for the CISP 400 Mandelbrot Set Project
 //----------------------------------------------//
-// Include important C++ libraries here
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <iostream>
-#include <sstream>
-#include <vector>
-#include <complex>
+
+//include our header file that has all of our important libraries
 #include "ComplexPlane.h"
 
-// Make code easier to type with "using namespace"
-using namespace sf;
-using namespace std;
-
+//start of our program
 int main()
 {
-    ComplexPlane y;
-    std::cout << y.test() << std::endl;
     return 0;
 }
