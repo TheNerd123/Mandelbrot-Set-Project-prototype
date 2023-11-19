79609c693d75e9c1697e2278d798ee952b952d3c
TheNerd123 <richardyin000@gmail.com>
2023-11-19 14:00:32 -0800

    idek anymore

---
+++ code/Mandelbrot_Set.cpp
@@ -0,0 +1,18 @@
+//Created by Richard Chao and Dominic Agoncillo
+//Created for the CISP 400 Mandelbrot Set Project
+//----------------------------------------------//
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+// Make code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+int main()
+{
+    return 0;
+}
